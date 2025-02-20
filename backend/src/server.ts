9f3c95237162c32bc453b74bab67355c6c85cde1
Mukhil <mukhil1140@gmail.com>
2025-02-19 21:43:53 -0500

    correcting spelling

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -24,7 +24,7 @@ const pool = new Pool({
 app.get("/tasks", authenticateJWT ,async (req, res) => {
   try {
     const userId = req.query.userId ? parseInt(req.query.userId as string) : null;
-    const query = userId ? "SELECT * FROM task WHERE userId = $1" : "SELECT * FROM task";
+    const query = userId ? "SELECT * FROM tasks WHERE userId = $1" : "SELECT * FROM task";
     const values = userId ? [userId] : [];
     const { rows } = await pool.query(query, values);
     res.json(rows);
@@ -39,7 +39,7 @@ app.post("/tasks", authenticateJWT,  async (req, res) => {
   try {
     const { title, description, id } = req.body;
     const { rows } = await pool.query(
-      "INSERT INTO task (title, description, userId) VALUES ($1, $2, $3) RETURNING *",
+      "INSERT INTO tasks (title, description, userId) VALUES ($1, $2, $3) RETURNING *",
       [title, description, id]
     );
     res.status(201).json(rows[0]);
@@ -55,7 +55,7 @@ app.put("/tasks/:id", authenticateJWT,  async (req, res) => {
     const { id } = req.params;
     const { title, description, iscomplete } = req.body;
     const { rows } = await pool.query(
-      "UPDATE task SET title = $1, description = $2, iscomplete = $3 WHERE id = $4 RETURNING *",
+      "UPDATE tasks SET title = $1, description = $2, iscomplete = $3 WHERE id = $4 RETURNING *",
       [title, description, iscomplete, id]
     );
     res.json(rows[0]);
@@ -69,7 +69,7 @@ app.put("/tasks/:id", authenticateJWT,  async (req, res) => {
 app.delete("/tasks/:id", authenticateJWT,  async (req, res) => {
   try {
     const { id } = req.params;
-    await pool.query("DELETE FROM task WHERE id = $1", [id]);
+    await pool.query("DELETE FROM tasks WHERE id = $1", [id]);
     res.status(204).send();
   } catch (error) {
     console.error(error);
